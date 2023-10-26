8cd0ee997009232e7400a89f3c76ed08784fcfe7
akeboshi <akeboshi82@gmail.com>
2023-10-26 15:20:30 +0800

    update code

--- src/coroutine/coroutine.py
+++ src/coroutine/coroutine.py
@@ -1,12 +1,11 @@
 import asyncio
-
+# 协程
 # 外部传入异步方法，返回一个Function对象，这个对象不是async_func本身
 def run(async_func):
     return asyncio.run(async_func())
 
 # 外部传入异步方法，并创建成为task，返回出去，指定返回task
 async def create_task(async_func):
-    asyncio.run()
     task = asyncio.create_task(async_func())
     return task
 
