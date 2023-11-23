00fae2a0204699f93f989f9368d7f55b76419150
akeboshi <akeboshi82@gmail.com>
2023-11-23 16:09:06 +0800

    add file tool code

---
+++ tools/file_tool.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+def copy_files(source_dir, destination_dir):
+    shutil.copy2(source_dir, destination_dir)
+
+# # 调用函数复制文件夹 A 中的所有文件到文件夹 B
+# copy_files("path/to/folderA", "path/to/folderB")
+
+def copy_tree(source_dir,destination_dir):
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+    shutil.copytree(source_dir,destination_dir)
+
+source = "/Users/gxm/workspace/editorclient/client/DigiPlayer/Builds/WebGL"
+destionation = "/Users/gxm/workspace/tq-editor/src/assets/unity"
+copy_tree(source,destionation)
