c07a04bd79814581ad6d2e759610cbd5b84f3cce
akeboshi <akeboshi82@gmail.com>
2023-12-04 16:08:49 +0800

    fix minutes res error

--- src/crawler.py
+++ src/crawler.py
@@ -11,7 +11,7 @@ def try_start():
      p1.start()
      
      # # 张江高科分时
-     p2 = Process(target=html.cycleStocksTime,args=(600036,"张江高科分时",enginstr,))
+     p2 = Process(target=html.cycleStocksTime,args=(600895,"张江高科分时",enginstr,))
      p2.daemon =True
      p2.start()
 
@@ -23,14 +23,14 @@ def try_start():
      # =============== 有些问题，分时数据由于是从第三方获取数据，不能同时拉取多个数据
      # 600036 招商银行
      # 招商收盘开盘量比等数据
-     p4 = Process(target=html.getStockData_datareader,args=('600036','招商银行',enginstr,))
-     p4.daemon = True
-     p4.start()
+     # p4 = Process(target=html.getStockData_datareader,args=('600036','招商银行',enginstr,))
+     # p4.daemon = True
+     # p4.start()
      
      # 600895 张江高科
      # 张江高科收盘开盘量比等数据
-     # p5 = Process(target=html.getStockData_datareader,args=('600895',"张江高科",enginstr,))
-     # p5.daemon = True
-     # p5.start()
+     p5 = Process(target=html.getStockData_datareader,args=('600895',"张江高科",enginstr,))
+     p5.daemon = True
+     p5.start()
      
      
