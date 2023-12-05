0c192d51d92715c834c5df7bbad14a9d83f47916
akeboshi <akeboshi82@gmail.com>
2023-12-05 17:14:29 +0800

    每日定时爬取数据

--- src/crawler.py
+++ src/crawler.py
@@ -1,11 +1,14 @@
 
 from multiprocessing import Process
 import src.html as html
+import time
+import schedule
+import threading
 enginstr = "mysql+pymysql://root:Akeboshi123~@localhost:3306/stock"
 
-def try_start():
-
-     # 招商分时
+done = threading.Event()
+def _runProcess():
+      # 招商分时
      p1 = Process(target=html.cycleStocksTime,args=(600036,"招商银行分时",enginstr,))
      p1.daemon =True
      p1.start()
@@ -20,17 +23,29 @@ def try_start():
      p3.daemon = True
      p3.start() 
      
-     # =============== 有些问题，分时数据由于是从第三方获取数据，不能同时拉取多个数据
      # 600036 招商银行
      # 招商收盘开盘量比等数据
-     # p4 = Process(target=html.getStockData_datareader,args=('600036','招商银行',enginstr,))
-     # p4.daemon = True
-     # p4.start()
+     p4 = Process(target=html.getStockData_datareader,args=('600036','招商银行',enginstr,))
+     p4.daemon = True
+     p4.start()
      
      # 600895 张江高科
      # 张江高科收盘开盘量比等数据
      p5 = Process(target=html.getStockData_datareader,args=('600895',"张江高科",enginstr,))
      p5.daemon = True
      p5.start()
+     done.set()
      
+def run_forever():
+     schedule.every().day.at("15:00").do(_runProcess)
+     while not done.is_set():
+       # localtime = src.timeutil.get_local_time()
+       # 每天15:00遍历一次网页的数据
+       schedule.run_pending()
+       time.sleep(1)
+
+def try_start():
+     thread = threading.Thread(target=run_forever,daemon=True)
+     thread.start() 
+
      
