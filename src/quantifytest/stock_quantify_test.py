0292af24cc1c854e696cf0f1fc9f367dc916f998
akeboshi <akeboshi82@gmail.com>
2024-03-14 11:35:02 +0800

    整理项目代码

--- src/quantifytest/stock_quantify_test.py
+++ src/quantifytest/stock_quantify_test.py
@@ -282,6 +282,8 @@ async def _check_total_stocks(
         date_part + " " + time_part, "%Y-%m-%d %H:%M:%S"
     ).time()
     for stockData in stockdatas:
+        if index > 20:
+            break
         # 获取分时级别数据
         stockNum = stockData["代码"].tolist()[0]
         # 分时数据
