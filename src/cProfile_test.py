5b91e16b264cc06d99cb4bda282cecabb73f8333
akeboshi <akeboshi82@gmail.com>
2024-04-17 16:34:43 +0800

    增加调试火焰图的生成

--- src/cProfile_test.py
+++ src/cProfile_test.py
@@ -4,6 +4,7 @@ def cProfile_test(func,*args,**kwargs):
     pr = cProfile.Profile()
     pr.enable()
     func(*args,**kwargs)
+    pr.dump_stats("pipeline.prof")
     pr.disable()
     s = io.StringIO()
     sortby = "cumtime"  # 仅适用于 3.6, 3.7 把这里改成常量了
